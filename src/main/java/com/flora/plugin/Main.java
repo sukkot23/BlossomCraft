e1f3cde0619b9c8ee463cc3227564de3989c3fdb
sukkot <mean02463@gmail.com>
2020-12-30 16:10:21 +0900

    Initial commit

--- src/main/java/com/flora/plugin/Main.java
+++ src/main/java/com/flora/plugin/Main.java
@@ -1,8 +1,6 @@
 package com.flora.plugin;
 
 import com.flora.api.BloomAPI;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
@@ -12,23 +10,11 @@ public class Main extends JavaPlugin
     @Override
     public void onEnable()
     {
-        try
-        {
-
+        try {
             BloomAPI.onLoadJsonLangFile();
             BloomAPI.onLoadMessageConfig();
-
-
-            for (Player p : Bukkit.getOnlinePlayers())
-            {
-                p.sendMessage("" + BloomAPI.messageMap.toString());
-            }
-
         }
 
         catch (FileNotFoundException e) { e.printStackTrace(); }
-
-
-
     }
 }
